b13f6d1fb796d8080177d8af45654a18917de95a
Rodion Gavrilov <rodiongavrilov@gmail.com>
2021-08-20 01:28:56 +0300

    [add]master

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $('.slider-list').slick({
+        arrows: false,
+        dots: true,
+        dotsClass: "slick-dots",
+        appendDots: '.appendDots'
+    });
+
+    $(window).on("scroll", function () {
+        $('.top-header').toggleClass("new", $(this).scrollTop() > 100);
+    });
+    
+});
